6f5d64213c2c8e3c55d2d0e2dcb7a86e7ad5cfe8
Piotr <piotrswierad.mail@gmail.com>
2022-12-04 20:22:10 +0100

    Day4

---
+++ Day4/Program.cs
@@ -0,0 +1,28 @@
+﻿var rawData = File.ReadAllLines("input.txt");
+
+var elfPairs = rawData.Select(x => x.Split(","));
+
+var overlapCompletelyPairs = 0;
+var overlapPartiallyPairs = 0;
+
+foreach (var elfPair in elfPairs)
+{
+    var firstElfSector = elfPair[0].Split("-").Select(x => Convert.ToInt32(x)).ToList();
+    var secondElfSector = elfPair[1].Split("-").Select(x => Convert.ToInt32(x)).ToList();
+
+    if ((firstElfSector[0] >= secondElfSector[0] && firstElfSector[1] <= secondElfSector[1])
+        || (firstElfSector[0] <= secondElfSector[0] && firstElfSector[1] >= secondElfSector[1]))
+    {
+        overlapCompletelyPairs++;
+    }
+
+    if (!(firstElfSector[1] < secondElfSector[0] || firstElfSector[0] > secondElfSector[1]))
+    {
+        overlapPartiallyPairs++;
+    }
+}
+
+Console.WriteLine($"Overlapping completely pairs: {overlapCompletelyPairs}");
+Console.WriteLine($"Overlapping partially pairs: {overlapPartiallyPairs}");
+
+Console.ReadLine();
